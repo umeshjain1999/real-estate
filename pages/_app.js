29d77ecec15df5e0ad02191726a2dd0027035da7
umesh <ujain2601@gmail.com>
2022-07-23 20:09:04 +0530

    feature : add progress on top to show page load

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,38 @@ import "../styles/globals.css";
 import "../styles/styleguide.css";
 import "../styles/main.scss";
 
+import NProgress from 'nprogress';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
-
 import "@fakeDb";
 
 function MyApp({ Component, pageProps }) {
+
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
+
   if (Component.getLayout) {
     return Component.getLayout(<Component {...pageProps} />);
   }
