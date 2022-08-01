1ecc8db48d2dd7c5a81c2b73d904adc5f1f49ee5
umesh <ujain2601@gmail.com>
2022-08-01 11:58:14 +0530

    feature : build proper icon system

--- next.config.js
+++ next.config.js
@@ -5,6 +5,25 @@ const nextConfig = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: {
+        loader: '@svgr/webpack',
+        options: {
+           svgoConfig: {
+              plugins: [{
+                 name: 'removeViewBox',
+                 active: false
+              }]
+           }
+        }
+     }
+    })
+
+    return config
+  },
   env: {
     REACT_APP_MOCK_API: process.env.REACT_APP_MOCK_API,
   },
