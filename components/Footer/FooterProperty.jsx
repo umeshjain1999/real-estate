ab954718d794c9a083419d7790a414a4cbc68980
umesh <ujain2601@gmail.com>
2022-08-17 20:23:28 +0530

    feat : add footer new section, remove not use file

---
+++ components/Footer/FooterProperty.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import Link from 'next/link'
+function FooterProperty() {
+  return (
+    <div className='footer-property'>
+      <div className="container">
+        <div className="footer-property-heading divider-sm">
+          {footerPropTitle}
+        </div>
+        <div className='divider-sm'>
+          <b>Property Type</b>{footerPropDesc}
+        </div>
+        {footerPropSaleLinks?.length ? <div className='divider-sm'>
+          <b>BHK in Navi Mumbai</b>{
+            footerPropSaleLinks.map((data, index) => {
+              return (
+                <Link href={data?.link} key={index}>
+                  <a className='footer-property-link'>{data?.title}</a>
+                </Link>
+              )
+            })
+          }
+        </div> : ''}
+        {footerPropSocitiesLinks?.length ? <div>
+          <b>Top Socities in Navi Mumbai</b>{
+            footerPropSocitiesLinks.map((data, index) => {
+              return (
+                <Link href={data?.link} key={index}>
+                  <a className='footer-property-link'>{data?.title}</a>
+                </Link>
+              )
+            })
+          }
+        </div> : ''}
+      </div>
+    </div>
+  )
+}
+
+export default FooterProperty
+
+const footerPropTitle = 'Popular Search in Navi Mumbai'
+
+const footerPropDesc = "in Navi Mumbai Flat for Sale in Navi Mumbai Shops for Sale in Navi Mumbai Builder Floors for Sale in Navi Mumbai Office Spaces for Sale in Navi Mumbai Independent Houses for Sale in Navi Mumbai"
+
+const footerPropSaleLinks = [
+  {
+    title: '1 BHK for Sale in Navi Mumbai',
+    link: '/projects'
+  },
+  {
+    title: '2 BHK for Sale in Navi Mumbai',
+    link: '/projects'
+  },
+  {
+    title: 'Studio for Sale in Navi Mumbai',
+    link: '/projects'
+  },
+  {
+    title: '3 BHK for Sale in Navi Mumbai',
+    link: '/projects'
+  },
+]
+const footerPropSocitiesLinks = [
+  {
+    title: 'Property for Sale in Rashmi complex',
+    link: '/projects'
+  },
+  {
+    title: 'Navi Mumbai Property for Sale in Pooja Apartment',
+    link: '/projects'
+  },
+  {
+    title: 'Navi Mumbai Property for Sale in Vrindavan Apartment',
+    link: '/projects'
+  },
+  {
+    title: 'Navi Mumbai Property for Sale in Kailash Tower',
+    link: '/projects'
+  },
+  {
+    title: 'Navi Mumbai Property for Sale in Shanti Lifespaces',
+    link: '/projects'
+  },
+  {
+    title: 'Mumbai Property for Sale in AV Crystal',
+    link: '/projects'
+  },
+]
--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,53 +1,57 @@
 import Logo from "@components/Logo";
 import Navigation from '@components/Navigation';
+import FooterProperty from "./FooterProperty";
 import { MOBILE_NUMBER, EMAIL } from '@constants/constant';
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div>
-          <div className="footer__wrapper">
-            <div className="footer__nav__wrap">
-              <span className="footer__wrapper__title">{footerNavigation.title}</span>
-              <Navigation navigation={footerNavigation.links} navChildClassName={'link-text'} />
+    <>
+      <FooterProperty />
+      <footer className="footer">
+        <div className="container">
+          <div>
+            <div className="footer__wrapper">
+              <div className="footer__nav__wrap">
+                <span className="footer__wrapper__title">{footerNavigation.title}</span>
+                <Navigation navigation={footerNavigation.links} navChildClassName={'link-text'} />
+              </div>
+              <div className="footer__nav__wrap">
+                <span className="footer__wrapper__title">{footerNavigation2.title}</span>
+                <Navigation navigation={footerNavigation2.links} navChildClassName={'link-text'} />
+              </div>
+              <div className="footer__nav__wrap">
+                <span className="footer__wrapper__title">{footerNavigation3.title}</span>
+                <Navigation navigation={footerNavigation3.links} navChildClassName={'link-text'} />
+              </div>
+              <div className="footer__nav__wrap">
+                <span className="footer__wrapper__title">{footerNavigation4.title}</span>
+                <Navigation navigation={footerNavigation4.links} navChildClassName={'link-text'} />
+              </div>
             </div>
-            <div className="footer__nav__wrap">
-              <span className="footer__wrapper__title">{footerNavigation2.title}</span>
-              <Navigation navigation={footerNavigation2.links} navChildClassName={'link-text'} />
+            <div className="footer__wrapper">
+              <div className="footer__nav__wrap footer-meta-desc">
+                <span className="footer__wrapper__title">{about.title}</span>
+                <p className="">{about.desc}</p>
+              </div>
+              <div className="footer__nav__wrap footer__nav__address">
+                <span className="footer__wrapper__title">{contactInfo.title}</span>
+                <address>
+                  <div className="footer__addr-txt normal-text">
+                    {contactInfo.mail}
+                  </div>
+                  <div className="footer__addr-txt normal-text">
+                    {contactInfo.tel}
+                  </div>
+                  <div className="footer__addr-txt normal-text">
+                    {contactInfo.address}
+                  </div>
+                </address>
+              </div>
+              <Logo variant='secondary' />
             </div>
-            <div className="footer__nav__wrap">
-              <span className="footer__wrapper__title">{footerNavigation3.title}</span>
-              <Navigation navigation={footerNavigation3.links} navChildClassName={'link-text'} />
-            </div>
-            <div className="footer__nav__wrap">
-              <span className="footer__wrapper__title">{footerNavigation4.title}</span>
-              <Navigation navigation={footerNavigation4.links} navChildClassName={'link-text'} />
-            </div>
-          </div>
-          <div className="footer__wrapper">
-            <div className="footer__nav__wrap footer-meta-desc">
-              <span className="footer__wrapper__title">{about.title}</span>
-              <p className="">{about.desc}</p>
-            </div>
-            <div className="footer__nav__wrap footer__nav__address">
-              <span className="footer__wrapper__title">{contactInfo.title}</span>
-              <address>
-                <div className="footer__addr-txt normal-text">
-                  {contactInfo.mail}
-                </div>
-                <div className="footer__addr-txt normal-text">
-                  {contactInfo.tel}
-                </div>
-                <div className="footer__addr-txt normal-text">
-                  {contactInfo.address}
-                </div>
-              </address>
-            </div>
-            <Logo variant='secondary' />
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
 //* Company
