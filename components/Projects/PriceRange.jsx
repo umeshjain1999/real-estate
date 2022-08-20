1638ea70fadf599569751f291f011a795473944b
umesh <ujain2601@gmail.com>
2022-08-20 13:40:18 +0530

    fix : price filter overlap with footer updated with mui popover element

--- components/Projects/PriceRange.jsx
+++ components/Projects/PriceRange.jsx
@@ -1,15 +1,15 @@
+import { useState, useEffect } from 'react';
+import { Popover } from '@mui/material';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+import Slider from '@mui/material/Slider';
 import Icon from '@components/Icon';
 import { CURRENCY } from "@constants/constant";
-import Slider from '@mui/material/Slider';
-import { useToggle } from 'hooks';
-import { useState, useEffect } from 'react';
 
 function PriceRange({
   handlePriceRange,
   defaultValue = [10, 50]
 }) {
   const [priceRange, setPriceRange] = useState([10, 50])
-  const { toggle, updateToggle } = useToggle()
 
   useEffect(() => {
     setPriceRange(defaultValue)
@@ -19,34 +19,47 @@ function PriceRange({
   const finalPriceRange = () => {
     let min = priceRange[0] * 1000
     let max = priceRange[1] * 1000
-    updateToggle()
     handlePriceRange(min, max)
   }
 
 
 
   return (
-    <div className='price-range-wrapper pos-rel dropdown-res'>
-      <div onClick={updateToggle} className='price-range-title'>
-        <div>Price</div>
-        <Icon icon='dropdownArrow' className={`${toggle && 'price-range-svg-active'}`} />
-      </div>
-      {toggle &&
-        <div className='price-range-modal'>
-          <div className='price-range-modal-top'>
-            <Slider
-              getAriaLabel={() => 'Price Range'}
-              value={priceRange}
-              onChange={(e, newValue) => setPriceRange(newValue)}
-              getAriaValueText={(value) => `${CURRENCY} ${value}`}
-            />
-            <div>Minimum : {priceRange[0] * 1000}</div>
-            <div>Maximum : {priceRange[1] * 1000}</div>
+    <PopupState variant='popover'>
+      {(popupState) => (
+        <div className='price-range-wrapper pos-rel dropdown-res'>
+          <div className='price-range-title' {...bindTrigger(popupState)}>
+            <div>Price</div>
+            <Icon icon='dropdownArrow' />
           </div>
-          <div onClick={finalPriceRange} className='common-btn price-range-btn'>Done</div>
-        </div>}
-      {toggle && <div onClick={updateToggle} className='price-range-modal-backdrop'></div>}
-    </div>
+          <Popover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center"
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center"
+            }}
+          >
+            <div className='price-range-modal'>
+              <div className='price-range-modal-top'>
+                <Slider
+                  getAriaLabel={() => 'Price Range'}
+                  value={priceRange}
+                  onChange={(e, newValue) => setPriceRange(newValue)}
+                  getAriaValueText={(value) => `${CURRENCY} ${value}`}
+                />
+                <div>Minimum : {priceRange[0] * 1000}</div>
+                <div>Maximum : {priceRange[1] * 1000}</div>
+              </div>
+              <div onClick={finalPriceRange} className='common-btn price-range-btn'>Done</div>
+            </div>
+          </Popover>
+        </div>
+      )}
+    </PopupState>
   )
 }
 
