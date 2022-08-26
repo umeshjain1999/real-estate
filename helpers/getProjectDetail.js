325c8ce63740596540f1084946f21d93b3d8b68c
umesh <ujain2601@gmail.com>
2022-08-26 23:00:11 +0530

    fix : route projects/:id leads to error for fake ids

--- helpers/getProjectDetail.js
+++ helpers/getProjectDetail.js
@@ -1,39 +1,51 @@
 import { GetAPI } from "helpers"
 export const getProjectDetail = async (projectId) => {
 
-   const projectDetail = await GetAPI(`store/products?id=${projectId}`)
+   try {
+      const projectDetail = await GetAPI(`store/products?id=${projectId}`)
 
-   const product = projectDetail?.products?.[0] || {}
+      if (projectDetail?.products?.length) {
 
-   const floorPlanAndPrice = product?.variants && product?.variants.map(data => {
-      return {
-         structure: data?.title,
-         price: data?.prices[0]?.amount || '',
-         area: data?.area ? `${data?.area}sqft` : '-'
+         const product = projectDetail?.products?.[0] || {}
+
+         const floorPlanAndPrice = product?.variants && product?.variants.map(data => {
+            return {
+               structure: data?.title,
+               price: data?.prices[0]?.amount || '',
+               area: data?.area ? `${data?.area}sqft` : '-'
+            }
+         })
+
+         const finalData = {
+            id: product?.id,
+            name: product?.title,
+            price: product?.price || 'none',
+            imageArr: product?.images ? product?.images.map(data => data.url) : [],
+            tags: product?.tags ? product?.tags.map(data => data.value) : [],
+            provider: product?.provider || 'by sqfthomes',
+            address: product?.addressDetails,
+            rooms: product?.bedrooms || false,
+            bathrooms: product?.bathroom || false,
+            parking: product?.parking || false,
+            area: product?.carpetArea || false,
+            overview: product?.overview || false,
+            aboutProject: product?.aboutProject || false,
+            aboutDevelopers: product?.builderDetails || false,
+            highlights: product?.highlights && product?.highlights.split(',') || false,
+            amenities: product?.amenities && product?.amenities.split(',') || false,
+            floorPlanAndPrice: floorPlanAndPrice || false,
+            saved: false,
+            variants: product?.variants || []
+         }
+
+         return finalData
+      } else {
+         return false
       }
-   })
 
-   const finalData = {
-      id: product?.id,
-      name: product?.title,
-      price: product?.price || 'none',
-      imageArr: product?.images ? product?.images.map(data => data.url) : [],
-      tags: product?.tags ? product?.tags.map(data => data.value) : [],
-      provider: product?.provider || 'by sqfthomes',
-      address: product?.addressDetails,
-      rooms: product?.bedrooms || false,
-      bathrooms: product?.bathroom || false,
-      parking: product?.parking || false,
-      area: product?.carpetArea || false,
-      overview: product?.overview || false,
-      aboutProject: product?.aboutProject || false,
-      aboutDevelopers: product?.builderDetails || false,
-      highlights: product?.highlights && product?.highlights.split(',') || false,
-      amenities: product?.amenities && product?.amenities.split(',') || false,
-      floorPlanAndPrice: floorPlanAndPrice || false,
-      saved: false,
-      variants: product?.variants || []
+   } catch (error) {
+      console.error('Encountered error', error)
+      return false
    }
 
-   return finalData
 }
