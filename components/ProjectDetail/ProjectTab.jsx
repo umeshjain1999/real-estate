8cf955aa0bc3d9c4ee1b3733d4ff96ba3ceec928
umesh <ujain2601@gmail.com>
2022-08-31 14:45:55 +0530

    feature : add sticky navigation project detail

--- components/ProjectDetail/ProjectTab.jsx
+++ components/ProjectDetail/ProjectTab.jsx
@@ -1,21 +1,31 @@
 import Link from 'next/link'
-
+import { useSticky } from 'hooks'
 function ProjectTab({
   projectTabs = []
 }) {
+  const { sticky, stickyRef } = useSticky();
 
   return (
-    <div className="project__tab">
-      {projectTabs && projectTabs.map((data,index) => {
-        if(data && data?.title && data?.data){
-          return (
-            <Link href={data?.link} key={index}>
-              <a className='project__tab-button'>{data?.title}</a>
-            </Link>
-          )
-        }
-      })}
-    </div>
+    <>
+      <div className={`project__tab ${sticky ? 'sticky' : ''}`} ref={stickyRef}>
+        {projectTabs && projectTabs.map((data, index) => {
+          if (data && data?.title && data?.data) {
+            return (
+              <Link href={data?.link} key={index}>
+                <a className='project__tab-button'>{data?.title}</a>
+              </Link>
+            )
+          }
+        })}
+      </div>
+      {sticky && (
+        <div
+          style={{
+            height: `${stickyRef.current?.clientHeight}px`
+          }}
+        />
+      )}
+    </>
   )
 }
 
--- hooks/index.js
+++ hooks/index.js
@@ -1,8 +1,10 @@
 import { useAuthContext } from "./useAuthContext";
 import { useLoginModalContext } from "./useLoginModalContext";
 import { useToggle } from "./useToggle";
+import useSticky from "./useSticky";
 export {
   useAuthContext,
   useLoginModalContext,
-  useToggle
+  useToggle,
+  useSticky
 }
